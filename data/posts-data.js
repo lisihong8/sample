d822f10f43f225d4d66c0cb0baa2f9645f07c34a
huanggengzhong <915835528@qq.com>
2020-04-28 12:07:09 +0800

    first commit

---
+++ data/posts-data.js
@@ -0,0 +1,63 @@
+
+
+// 按住  alt+shift+F 可以格式化代码格式
+
+var local_database = [{
+  date: "Nov 12 2018",
+  title: "正是虾肥蟹壮时",
+  imgSrc: "/images/post/crab.png",
+  avatar: "/images/avatar/1.png",
+  content: "久闻七月老师大名，终于能有机会跟着老师学习。看了前两章觉得老师非常负责尤其那句‘只要是我七月出得课程，我就一定会持续维护下去’。在学习的路上有这样的老师陪伴真的是特别安心。",
+  reading: '112',
+  collection: '96',
+
+}, {
+  date: "Nov 12 2018",
+  title: "正是虾肥蟹壮时",
+  imgSrc: "/images/post/bl.png",
+  avatar: "/images/avatar/2.png",
+  content: "久闻七月老师大名，终于能有机会跟着老师学习。看了前两章觉得老师非常负责尤其那句‘只要是我七月出得课程，我就一定会持续维护下去’。在学习的路上有这样的老师陪伴真的是特别安心。",
+  reading: '145',
+  collection: '89',
+
+}, {
+  date: "Nov 12 2018",
+  title: "正是虾肥蟹壮时",
+  imgSrc: "/images/post/cat.png",
+  avatar: "/images/avatar/3.png",
+  content: "久闻七月老师大名，终于能有机会跟着老师学习。看了前两章觉得老师非常负责尤其那句‘只要是我七月出得课程，我就一定会持续维护下去’。在学习的路上有这样的老师陪伴真的是特别安心。",
+  reading: '232',
+  collection: '196',
+
+}, {
+  date: "Nov 12 2018",
+  title: "正是虾肥蟹壮时",
+  imgSrc: "/images/post/sls.jpg",
+  avatar: "/images/avatar/4.png",
+  content: "久闻七月老师大名，终于能有机会跟着老师学习。看了前两章觉得老师非常负责尤其那句‘只要是我七月出得课程，我就一定会持续维护下去’。在学习的路上有这样的老师陪伴真的是特别安心。",
+  reading: '112',
+  collection: '96',
+
+}, {
+  date: "Nov 12 2018",
+  title: "正是虾肥蟹壮时",
+  imgSrc: "/images/post/vr.png",
+  avatar: "/images/avatar/5.png",
+  content: "久闻七月老师大名，终于能有机会跟着老师学习。看了前两章觉得老师非常负责尤其那句‘只要是我七月出得课程，我就一定会持续维护下去’。在学习的路上有这样的老师陪伴真的是特别安心。",
+  reading: '121',
+  collection: '45',
+
+}, {
+  date: "Nov 12 2018",
+  title: "正是虾肥蟹壮时",
+  imgSrc: "/images/post/xiaolong.jpg",
+  avatar: "/images/avatar/avatar.png",
+  content: "久闻七月老师大名，终于能有机会跟着老师学习。看了前两章觉得老师非常负责尤其那句‘只要是我七月出得课程，我就一定会持续维护下去’。在学习的路上有这样的老师陪伴真的是特别安心。",
+  reading: '312',
+  collection: '296',
+
+}];
+
+module.exports = {
+  postList: local_database
+}
---
+++ pages/welcome/welcome.js
@@ -0,0 +1,24 @@
+Page({
+
+  onTap: function() {
+    //console.log("onTap");
+
+    // wx.navigateTo({
+    //   url: '../posts/posts'
+    // })
+
+    wx.redirectTo({
+      url: '../posts/posts'
+    })
+
+  },
+
+  onHide: function() {
+    console.log('welcome page is Hide');
+  },
+
+  onUnload: function() {
+    console.log('welcome page is unload');
+  }
+
+})
